f8772d1dc8426c194da77bde92d5b47039f9589c
skbarbee <barbee.s.k@gmail.com>
2022-10-03 18:18:36 -0400

    local server is running

--- server.js
+++ server.js
@@ -6,3 +6,54 @@ const express = require("express") // import express
 const morgan = require("morgan") // import morgan
 const mongoose = require("mongoose") // import mongoose
 const path = require("path") // import path module
+
+/////////////////////////////////////////////
+// Import Our Models
+/////////////////////////////////////////////
+const Student = require('./models/character')
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG)
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error))
+/////////////////////////////////////////////
+// Create our Express Application Object
+/////////////////////////////////////////////
+const app = express()
+
+/////////////////////////////////////////////
+//Middleware
+/////////////////////////////////////////////
+
+app.use(morgan("tiny")) //logging
+app.use(express.urlencoded({ extended: true })) // parse urlencoded request bodies
+app.use(express.static("public")) // serve files from public statically
+app.use(express.json()) // parses incoming requests with JSON payloads
+
+/////////////////////////////////////////////
+//Routes
+/////////////////////////////////////////////
+
+app.get("/",(req, res)=>{
+	res.send("your server is running ... better catch it")
+})
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`))
