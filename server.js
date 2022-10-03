52ac436d67c6689487640b76b08183c65b4bed81
skbarbee <barbee.s.k@gmail.com>
2022-10-03 17:24:53 -0400

    intalled node modules and imported dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config() // Load ENV Variables
+const express = require("express") // import express
+const morgan = require("morgan") // import morgan
+const mongoose = require("mongoose") // import mongoose
+const path = require("path") // import path module
